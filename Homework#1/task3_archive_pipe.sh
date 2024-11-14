1c662c0a75666a2cd143bff62f6efc25ac4856e7
badralboushy <badralboushy.scholar@gmail.com>
2024-11-14 07:10:10 +0300

    review the files

--- Homework#1/task3_archive_pipe.sh
+++ Homework#1/task3_archive_pipe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# create a pip if 
+# create the pipe 
 mkfifo my_pipe
 
 echo "/var/log/messages" > my_pipe &
@@ -11,5 +11,4 @@ tar -cf archive.tar -T my_pipe
 
 # to decompress the archive with the directory tree
 # to remove the directory tree of the file we add --strip-components=<number of nested directories>
-
 # tar -xf archive.tar
